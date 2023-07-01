da7b0c1c026ab25c8c14388458bb0864c6bb6b5f
autodoc <alexeym@pkwteile.de>
2023-07-01 18:45:03 +0300

    changing in singleton example

--- src/main/java/patterns/generative/singleton/ConnectToDB.java
+++ src/main/java/patterns/generative/singleton/ConnectToDB.java
@@ -2,15 +2,16 @@ package patterns.generative.singleton;
 
 public class ConnectToDB {
 
-    private static final ConnectToDB connectToDB = new ConnectToDB();
+    private static ConnectToDB connectToDB;
 
     private ConnectToDB() {
         System.out.println("new DB");
     }
 
     public static synchronized ConnectToDB getConnectToDB() {
+        if (connectToDB == null) {
+            connectToDB = new ConnectToDB();
+        }
         return connectToDB;
     }
-
-
 }
--- src/main/java/patterns/generative/singleton/Launch.java
+++ src/main/java/patterns/generative/singleton/Launch.java
@@ -3,6 +3,8 @@ package patterns.generative.singleton;
 // позволяет создать экзепляр класса только один раз
 public class Launch {
     public static void main(String[] args) {
+
         ConnectToDB connectToDB = ConnectToDB.getConnectToDB();
+
     }
 }
